90c326c226ec550f0207b29b650c51f327b192bd
Piotr Staniów <staniowp@gmail.com>
2018-10-21 11:05:59 +0200

    Code::Dive Conference

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+const pug = {
+    test: /\.pug$/,
+    use: ['html-loader?attrs=false', 'pug-html-loader'],
+};
+
+const css = {
+    test: /\.css$/,
+    use: [
+        'style-loader',
+        'css-loader',
+    ]
+};
+
+const fonts = {
+    test: /\.(woff|woff2|eot|ttf|otf)$/,
+    use: [
+        'file-loader'
+    ]
+};
+
+module.exports = {
+    entry: './index.js',
+    devServer: {
+        hot: true,
+    },
+    output: {
+        path: path.resolve(__dirname, 'docs'),
+        filename: '[name].bundle.js',
+    },
+    module: {
+        rules: [pug, css, fonts],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './index.pug',
+            inject: false,
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new CopyWebpackPlugin([{ from: 'img', to: 'img' }])
+    ]
+};
